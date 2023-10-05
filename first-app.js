11817ad5f01337460b32294f48d877cadaa3bec3
Saveen-ProminentPixel <saveen.p@prominentpixel.com>
2023-10-05 19:02:17 +0530

    updated app

--- first-app.js
+++
@@ -1,3 +0,0 @@
-// console.log("Hello World from Node.js");
-const fs = require("fs");
-fs.writeFileSync("hello.txt", "Hello from Node.js");
--- routes/admin.js
+++ routes/admin.js
@@ -2,26 +2,23 @@ const path = require("path");
 
 const express = require("express");
 
-const rootDir = require("../util/path");
+const adminController = require("../controllers/admin");
 
 const router = express.Router();
 
-const products = [];
+// /admin/add-product => GET
+router.get("/add-product", adminController.getAddProduct);
 
-router.get("/add-product", (req, res, next) => {
-  //   console.log("In another the middlware");
-  //   res.sendFile(path.join(rootDir, "views", "add-product.html"));
-  res.render("add-product", {
-    pageTitle: "Add Product",
-    path: "/admin/add-product",
-  });
-});
+// /admin/products => GET
+router.get("/products", adminController.getProducts);
 
-router.post("/add-product", (req, res, next) => {
-  products.push({ title: req.body.title });
-  console.log(req.body);
-  res.redirect("/");
-});
+// /admin/add-product => POST
+router.post("/add-product", adminController.postAddProduct);
 
-exports.routes = router;
-exports.products = products;
+router.get("/edit-product/:productId", adminController.getEditProduct);
+
+router.post("/edit-product", adminController.postEditProduct);
+
+router.post("/delete-product", adminController.postDeleteProduct);
+
+module.exports = router;
